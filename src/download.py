870e2c589f50a76e706a2124c5195ed87d609d54
范希波 <97569336+fanxibo@users.noreply.github.com>
2026-06-18 21:41:02 +0800

    Update download.py

--- src/download.py
+++ src/download.py
@@ -4,7 +4,7 @@ from modelscope.hub.snapshot_download import snapshot_download
 print("正在登录魔搭账号...")
 api = HubApi()
 # 注入你的 Token
-api.login('ms-7a7fb55b-cce0-4d1a-8585-113b72f85047')
+os.getenv("MODELSCOPE_TOKEN")
 
 print("登录成功，开始下载 DDL_X 数据集 (支持断点续传)...")
 dataset_dir = snapshot_download('DDLteam/DDL_X', repo_type='dataset', local_dir='./DDL_X_dataset')
